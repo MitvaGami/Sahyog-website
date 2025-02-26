5bd60b3db3f562f9bc41e3971e45463724f17fa9
MitvaGami <156935991+MitvaGami@users.noreply.github.com>
2025-02-26 10:59:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export const navItems = [
+{label: "About", href: "#"},
+{label: "Campaigns", href: "#"},
+{label: "Events", href: "#"},
+{label: "Donate", href: "#"},
+{label: "Get Involved", href: "#"},
+{label: "Profile", href: "#"},
+];
